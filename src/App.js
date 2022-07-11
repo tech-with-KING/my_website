0525af1b6ce5cfc5fac7c3951fc0dd20d91f1105
cosmic <kingsleyfrancis42@gmail.com>
2022-07-11 05:16:21 -0400

    any edits should go here

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import Body from './body/body';
 import Menue_Bar from './nav/menue';
-import Navbar from './nav/nav';
 import MenuIcon from '@mui/icons-material/Menu';
-import HomeIcon from '@mui/icons-material/Home';
-import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
-import CreditScoreIcon from '@mui/icons-material/CreditScore';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
-import MenuBookTwoToneIcon from '@mui/icons-material/MenuBookTwoTone';
 import ClearTwoToneIcon from '@mui/icons-material/ClearTwoTone';
-import InfoTwoToneIcon from '@mui/icons-material/InfoTwoTone';
 import DownBar from './root/bottom_bar';
-
+import Projects from "./projects_page/index"
 
 class App extends Component {
   state={
@@ -38,7 +32,7 @@ class App extends Component {
                 <div className='ico' onClick={()=>{settoggle()}}>{!toggle?<MenuIcon style={{fontSize:'30px',fontWeight:'bold',transition:"0.5s ease-in"}} /> :<ClearTwoToneIcon  style={{fontSize:'30px',fontWeight:'bold',transition:"0.5s ease-in"}}/>}</div>
                 <div  className='light'><DarkModeIcon style={{fontSize:'30px',fontWeight:'bold',transition:"0.5s ease-in"}} /></div>
         </div>
-        
+        <Projects/>
         <Body></Body>
         <DownBar></DownBar>
        </>
--- src/projects_page/index.js
+++ src/projects_page/index.js
@@ -1,106 +1,22 @@
 import React from 'react';
-import './body.css'
+import './index.css'
 import { useState,useEffect } from 'react';
 import { motion } from 'framer-motion'
-import Carded_Two from '../components/blogcard/bob2';
 import {useInView} from "react-intersection-observer"
 import {useAnimation} from "framer-motion"
 const Projects=(props)=>{
     const [toggle,settoggle]=useState(false)
     const {ref:article_ref,inView:article_view}=useInView()
     const {ref:projec_ref,inView:project_view}=useInView()
-    const [dark,setdark]=useState(false)
-    const darkmode = () =>{
-        if (dark) {
-            setdark(false)
-        } else {
-            setdark(true)
-        }
-    }
-    const coloring={
-      darkbg:'black',
-      lightbg:'white',
-      darkcl:'black',
-      lightcl:'white',
-      darkbd:'',
-      lightbd:''
-  }
-  const datas =[
-      {
-          id:1,
-          text:"this is going to be some of the best articles yuo will ever get to read",
-          img:'studio'
-      },
-      {
-        id:2,
-        text:"this is going to be some of the best articles yuo will ever get to read",
-        img:'studio'
-    },
-    {
-        id:3,
-        text:"this is going to be some of the best articles yuo will ever get to read",
-         img:'studio'
-    }
-  ]
-
     return(
-        <div className='body'>
-            <motion.div className='hot'
-	      initial={{opacity:0, translateX:-200}}
-              transition={{duration:1}}
-             >
-             <h1>Projects</h1>
-
-            </motion.div> 
-            <div className='my_projects'>
-                <div  className='eop'>
-		    <div ref={projec_ref} className="p_div">
-			   {
-                    datas.map((data,i)=>{
-                        return(
-                            <motion.div key={data.id}
-			         initial={{translateX:"-100vw"}}
-				animate={project_view?{opacity:1, translateX: 0}:{}}
-					transition= {{duration:1.5,delay:i*1.5}} > 
-                                <Carded /> 
-                            </motion.div>
-                        )
-                    })
-                }
-			
-		    </div>
-		 
-
-                </div>
-             
-                <div className='line'></div>
-            
-           
-            
-            <motion.div  className='hots'
-			            >
-             <h1>Articles</h1>
-
-            </motion.div>
-            
-                <div ref={article_ref}>
-                {
-                    datas.map((data,i)=>{
-                        return(
-			    <motion.div
-                                key={data.id}
-			         initial={{opacity:0, translateX:-100}}
-				animate={article_view?{opacity:1, translateX: 0}:{}}
-				transition={{duration:1,delay:i*1}} >
-                                <Carded_Two />
-                            </motion.div>
-                        
-                        )
-                    })
-                }
-		    </div>
-            </div>
-               
+        <div className='project_container'>
+            <div className='project_grid'>
+	    <div className='first_element'></div>
+	    <div></div>
+	    <div></div>
+	    <div></div>
+	    <div></div>
+	    </div>
         </div>    
     )
 }
