5f988a66115fa47f0c93fa0f22be0a7e65159c3f
cosmic <kingsleyfrancis42@gmail.com>
2022-07-11 20:54:41 -0400

    updated the ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Body from './body/body';
+import Articles from "./article_page/index"
 import Menue_Bar from './nav/menue';
 import MenuIcon from '@mui/icons-material/Menu';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
@@ -9,35 +10,36 @@ import DownBar from './root/bottom_bar';
 import Projects from "./projects_page/index"
 
 class App extends Component {
-  state={
-    toggle:false
-  }
-  
-  render() {
-    const {toggle}=this.state
-    const settoggle=()=>{
-      if (toggle) {
-        this.setState({toggle:false})
-      } else {
-        this.setState({toggle:true})
-      }
-    }
-   
-    return (
-       <>
-       <Menue_Bar  toggle={toggle}/>
-  
-       <div className='top_bar'  >
+	state = {
+		toggle: false
+	}
 
-                <div className='ico' onClick={()=>{settoggle()}}>{!toggle?<MenuIcon style={{fontSize:'30px',fontWeight:'bold',transition:"0.5s ease-in"}} /> :<ClearTwoToneIcon  style={{fontSize:'30px',fontWeight:'bold',transition:"0.5s ease-in"}}/>}</div>
-                <div  className='light'><DarkModeIcon style={{fontSize:'30px',fontWeight:'bold',transition:"0.5s ease-in"}} /></div>
-        </div>
-        <Projects/>
-        <Body></Body>
-        <DownBar></DownBar>
-       </>
-    );
-  }
+	render() {
+		const { toggle } = this.state
+		const settoggle = () => {
+			if (toggle) {
+				this.setState({ toggle: false })
+			} else {
+				this.setState({ toggle: true })
+			}
+		}
+
+		return (
+			<>
+			{/*	<Menue_Bar toggle={toggle} />*/}
+
+				<div className='top_bar'  >
+
+					<div className='ico' onClick={() => { settoggle() }}>{!toggle ? <MenuIcon style={{ fontSize: '30px', fontWeight: 'bold', transition: "0.5s ease-in" }} /> : <ClearTwoToneIcon style={{ fontSize: '30px', fontWeight: 'bold', transition: "0.5s ease-in" }} />}</div>
+					<div className='light'><DarkModeIcon style={{ fontSize: '30px', fontWeight: 'bold', transition: "0.5s ease-in" }} /></div>
+				</div>
+				<Projects />
+			{/*	<Body></Body>*/}
+			{/*<Articles />*/}
+				<DownBar></DownBar>
+			</>
+		);
+	}
 }
 
 export default App;
--- src/article_page/index.js
+++ src/article_page/index.js
@@ -1,1 +1,35 @@
-imp
+import React from 'react';
+import './index.css'
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion'
+import { useInView } from "react-intersection-observer"
+import { useAnimation } from "framer-motion"
+const Articles = (props) => {
+	const [toggle, settoggle] = useState(false)
+	const { ref: article_ref, inView: article_view } = useInView()
+	const { ref: projec_ref, inView: project_view } = useInView()
+	return (
+		<div className='articulate'>
+
+			<div className='articlate_con'>
+				<p>
+					lorem20 dkddd ff djdjfidj fdjfidjf d djfsdus sdj bjddjdjdkdud sjfdfjf bdjfkdkdjf dfsjfdjfldidj
+
+				</p>
+			</div>
+			<div className='list_menue'>
+				<ul>
+					<li>this day in june</li>
+					<li> this day in may</li>
+					<li> from home with love</li>
+					<li> fight for your right</li>
+				</ul>
+			</div>
+
+
+		</div>
+	)
+}
+export default Articles;
+
+
