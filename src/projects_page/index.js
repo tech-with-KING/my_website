057fa8fd6568acc459e78eb532698b3c1fb06bc6
cosmic <kingsleyfrancis42@gmail.com>
2022-08-18 07:34:03 -0400

    this are the linus trovald thought of when he invented the system

--- src/projects_page/index.js
+++ src/projects_page/index.js
@@ -9,15 +9,49 @@ const Projects=(props)=>{
     const {ref:article_ref,inView:article_view}=useInView()
     const {ref:projec_ref,inView:project_view}=useInView()
     return(
-        <div className='project_container'>
-            <div className='project_grid'>
-	    <div className='first_element'></div>
-	    <div></div>
-	    <div></div>
-
-	    <div className='view_on_vercel'></div>
-	    </div>
-        </div>    
+	    <div className='project_container'>
+	    			<div className='project_grid'>
+				<div className='first_element'></div>
+				<div></div>
+				<div></div>
+				<div className='view_on_vercel'>
+					<button>
+						View on Vercel
+					</button>
+					<p>
+						this is a brief description of this pr
+						oject that i have done
+						thiis it so that it can be used to help the lifes of
+						students in the universit
+						y of lagos state
+					</p>
+					<button>
+						View on Vercel
+					</button>
+				</div>
+			</div>
+			<div className='project_grid'>
+				<div className='first_element'></div>
+				<div></div>
+				<div></div>
+				<div className='view_on_vercel'>
+					<button>
+						View on Vercel
+					</button>
+					<p>
+						this is a brief description of this pr
+						oject that i have done
+						thiis it so that it can be used to help the lifes of
+						students in the universit
+						y of lagos state
+					</p>
+					<button>
+						View on Vercel
+					</button>
+				</div>
+			</div>
+		</div>
+	    
     )
 }
 export default Projects;
