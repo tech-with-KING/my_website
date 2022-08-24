a33bf87924cf7341336a9e8a3837411775080dc3
cosmic <kingsleyfrancis42@gmail.com>
2022-08-24 06:15:20 -0400

    i finally started saying the right words about myself to the people of the world

--- src/body/about.js
+++ src/body/about.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import imgr from "./sc/mo_prev_ui.png"
 import './about.css'
 const About=(props)=>{
     const {toggle}=props
@@ -12,7 +13,7 @@ const About=(props)=>{
     return(
 	   
 <div className="content">
-  <img className="left-side" src="https://placeimg.com/640/480/any" alt="Any Image"></img>
+            <img className="left-side" src={imgr} alt="Any Image"></img>
   <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
     elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
     the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
@@ -30,12 +31,6 @@ const About=(props)=>{
   <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
     elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
     the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
-
-  <img classNameName="right-side" src="https://placeimg.com/640/480/any" alt="Any Image"></img>
-
-  <img classNameName="right-side" src="https://placeimg.com/640/480/any" alt="Any Image"></img>
-
-
   <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
     elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
     the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
@@ -51,9 +46,6 @@ const About=(props)=>{
   <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
     elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
     the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
-
-            <img classNameName="right-side" src="https://placeimg.com/640/480/any" alt="Any Image"></img>
-
   <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
     elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
     the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
