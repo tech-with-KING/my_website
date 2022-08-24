9829b52a2227a2c5d66534b012718f071975fff7
cosmic <kingsleyfrancis42@gmail.com>
2022-08-24 06:32:00 -0400

    into a new verse

--- src/body/about.js
+++ src/body/about.js
@@ -14,42 +14,14 @@ const About=(props)=>{
 	   
 <div className="content">
             <img className="left-side" src={imgr} alt="Any Image"></img>
-            <h1 style={{color:"white"}}> About me</h1>
+            <h1 style={{color:"whiteo"}}> About me</h1>
   <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
     elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
-    the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
-  <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
+    the position of other elements and other elements will not affect them, whether they touch each other or not.
+  In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
     elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
     the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
-  <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
-    elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
-    the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
-
-  <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
-    elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
-    the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
-
-  <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
-    elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
-    the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
-  <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
-    elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
-    the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
-
-  <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
-    elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
-    the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
-
-  <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
-    elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
-    the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
-
-  <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
-    elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
-    the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
-  <p>In web design, page elements with the CSS float property applied to them are just like the images in the print layout where the text flows around them. Floated elements remain a part of the flow of the web page. This is distinctly different than page
-    elements that use absolute positioning. Absolutely positioned page elements are removed from the flow of the webpage, like when the text box in the print layout was told to ignore the page wrap. Absolutely positioned page elements will not affect
-    the position of other elements and other elements will not affect them, whether they touch each other or not.</p>
+ 
 
 </div>
  
