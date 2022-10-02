b811910a0504110252b003e3237451b528a387e5
cosmic <kingsleyfrancis42@gmail.com>
2022-10-03 00:24:13 +0100

    not here

--- src/components/cardbar/cardbar.js
+++ src/components/cardbar/cardbar.js
@@ -43,11 +43,10 @@ class Card_componnt extends Component {
         <>
 		<div className="card_to" style={{minHeight:'400px'}}>
                 <img src={img} alt="John" style={{width:'130px',height:'130px',borderRadius:'50%',}}/>
-                <h1 className="about_heading">Hi &#x1F44B; I am Kingley</h1>
-                <p className="title">Full-Stack Developer</p>
+                <h3 className="about_heading">Hi &#x1F44B;&#x1F600;  I am Kingley</h3>
+                <p className="title">A Full-Stack Developer</p>
 				<p className='about_short_text'>
-		And I’ve spent the last three years learning everything there is to know about Software Designs – from Great App/Web
-	    UI designs with Reactjs to To building Restful Apis with Django and Nodejs. I’m currently applying this knowledge in my role as a full-stack Developer at Algophile.com a coming of Age AI task automation system.I love workin on new tasks and collaborating to build great projects so I am open to new opportunities &#x1F44F;.
+		And I’ve spent the last three years learning and building everything  there is to Web and  Software Designs – from Great App/Web UI designs with Reactjs to To building fast and secure Restful Apis with Django and Nodejs. I’m currently applying this knowledge in my role as a full-stack Developer at Algophile.com a coming of Age AI task automation system.I love working on new tasks and collaborating to build great projects so I am open to new opportunities &#x1F44F;.
 		I’m a solution-oriented Full-Stack Web Developer  with a liking for Building web interfaces ,Maths ,Technical Writing &#x1F4DD;, Music &#x1F3B8;, Basket-ball  and Reading &#x1F4DA;.. See some of my very interesting project ideas and articles below &#128640; ; .
 
 		</p>
--- src/root/bottom_bar.js
+++ src/root/bottom_bar.js
@@ -1,9 +1,42 @@
+import {MailOutline,Message } from '@mui/icons-material';
+import {motion} from 'framer-motion'
 import React from 'react';
 import './bottom_bar.css'
-import cosmic from '../img/s_tile.svg'
-import {FacebookSharp,GitHub, Twitter, LinkedIn, Instagram, ArrowDownward, ViewCarousel, ArrowDropUp, Email, YouTube, Message} from '@mui/icons-material';
+import {GitHub, Twitter, LinkedIn, ViewCarousel, ArrowDropUp, Email, YouTube} from '@mui/icons-material';
 
 const DownBar=()=>{
+            const animae=[
+	    	
+
+
+            {
+                id:1,
+		color:'about_heading',
+                item:<GitHub />
+            },
+           {
+               id:2,
+	       color:'about_heading',
+               item: <Twitter style={{color:'#00ACEE'}}/>,
+           },
+           {
+               id:3,
+	       color:'about_heading',
+               item:<MailOutline style={{color:'#DB4437'}}/>
+		   
+           },
+	    
+        {
+            id:4,
+	    color:'about_heading',
+            item:<LinkedIn style={{color:'#0072b1'}} />
+        },
+	    {
+		id:5,
+		color:'about_heading',
+		item:  <YouTube style={{color:'#FF0000'}}/>
+            }
+        ]
     return(
         <div className='downcomponent'>
             <div className='extra_contents'>
@@ -16,18 +49,25 @@ const DownBar=()=>{
                     <li>About</li>
                     <li>Contact</li>
                 </ul>
-                <img className='logo' src={cosmic} alt='logo'/>
+                <img className='logo' src='/img/mormon.png' alt='logo'/>
             </div>
             <div className='our_socials'>
               <>
               <button>Contact Us :</button>
             <ul>
-            <li><YouTube style={{fontSize:'70px',width:'30px', height:'30px',color:'white',backgroundColor:'#000020'}}/></li>
-	    <li><Email style={{fontSize:'70px',width:'30px', height:'30px',color:'white',backgroundColor:'#000020'}}/></li>
-            <li><LinkedIn style={{fontSize:'70px',width:'30px', height:'30px',color:'white',backgroundColor:'#000020'}}/></li>
-	    <li><GitHub style={{fontSize:'70px',width:'30px', height:'30px',color:'white',backgroundColor:'#000020'}}/></li>
-            <li><Twitter style={{fontSize:'70px',width:'30px', height:'30px',color:'white',backgroundColor:'#000020'}}/></li>
-                </ul>
+	                    {
+                    animae.map((anim,i)=>{
+                        return(
+                            <motion.div href="#" className='div'
+                                key={anim.id}
+                                initial={{opacity:0, translateX:-40}}
+                                animate={{opacity:1, translateX: 0}}
+                                transition={{duration:0.5,delay:i*0.5}}
+                            >{anim.item }</motion.div>
+                        )
+                    })
+                }
+	    </ul>
             </>
 	    <div  className='modals'>
             <div className='goto_top'>
